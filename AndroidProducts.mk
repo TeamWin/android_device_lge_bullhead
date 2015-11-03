f7a426bcf7dafa2366c27366f613527089b9dd8f
Jake Whatley <jacob.whatley91@gmail.com>
2015-11-02 19:48:11 -0500

    bullhead: setup omni makefiles
    Change-Id: I8a1020a38d888098fdf5562baedd747933120c76

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,5 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_bullhead.mk
+    $(LOCAL_DIR)/aosp_bullhead.mk \
+    $(LOCAL_DIR)/omni_bullhead.mk
---
+++ omni_bullhead.mk
@@ -0,0 +1,22 @@
+# Inherit base AOSP device configuration
+$(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
+
+# Inherit APNs list
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Bootanimation
+TARGET_BOOTANIMATION_SIZE := 1080x720
+
+# Override product naming for Omni
+PRODUCT_NAME := omni_bullhead
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 5x
+PRODUCT_MANUFACTURER := LGE
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="google/bullhead/bullhead:6.0/MDB08M/2353240:user/release-keys" \
+    PRIVATE_BUILD_DESC="bullhead-user 6.0 MDB08M 2353240 release-keys"
