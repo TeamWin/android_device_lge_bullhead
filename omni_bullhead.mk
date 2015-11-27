97f7f28acbcbf994e51b7196d458a47eed006f05
Jake Whatley <jacob.whatley91@gmail.com>
2015-11-27 08:00:42 +0100

    add selinux prop value
    Change-Id: Ie02be5ef91b03fc8ef4d9298f438be80ff369fbb

--- omni_bullhead.mk
+++ omni_bullhead.mk
@@ -17,6 +17,10 @@ TARGET_BOOTANIMATION_SIZE := 960x640
 PRODUCT_PROPERTY_OVERRIDES += \
     net.tethering.noprovisioning=true
 
+# SELinux
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.selinux=1
+
 # TWRP
 PRODUCT_COPY_FILES += \
     device/lge/bullhead/twrp.fstab:recovery/root/etc/twrp.fstab
