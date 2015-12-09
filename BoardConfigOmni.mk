b2ac4aa44b7fa58b3a3019372a9ced41c199915b
Jake Whatley <jacob.whatley91@gmail.com>
2015-12-08 22:33:52 -0500

    Revert "bullhead: do not include DSPManager"
    This reverts commit d051a4f11571944fb86d13126f7ce79a3d92bf83.
    
    Change-Id: I02b1523bbc211f2415e2474a7411875a4ecb31d2

--- BoardConfigOmni.mk
+++ BoardConfigOmni.mk
@@ -3,9 +3,6 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_SOURCE := kernel/lge/bullhead
 TARGET_KERNEL_CONFIG := bullhead_defconfig
 
-# Do not include DSPManager
-TARGET_NO_DSPMANAGER := true
-
 # Enable vendor image symlink
 BOARD_NEEDS_VENDORIMAGE_SYMLINK := true
 
