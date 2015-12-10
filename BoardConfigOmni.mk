9e76004259dd46fb684123066be205f1a0f89fd8
Jake Whatley <jacob.whatley91@gmail.com>
2015-12-09 20:06:23 -0500

    bullhead: use device's audio_effects.conf
    Change-Id: Ic7788ef1c855a0dc49bad8974f2abe964987266f

--- BoardConfigOmni.mk
+++ BoardConfigOmni.mk
@@ -3,6 +3,9 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_SOURCE := kernel/lge/bullhead
 TARGET_KERNEL_CONFIG := bullhead_defconfig
 
+# Use device's audio_effects.conf
+TARGET_USE_DEVICE_AUDIO_EFFECTS_CONF := true
+
 # Enable vendor image symlink
 BOARD_NEEDS_VENDORIMAGE_SYMLINK := true
 
